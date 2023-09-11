7b5abb97cd0e2f0327c3189cf3c6ec106dac3f16
TrizzyFroze <trizzyfroze2000@gmail.com>
2023-09-11 16:38:33 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - prints all possible combinations of two two-digit
- *
  * Return: Always 0.
- *
  */
 int main(void)
 {
         int num1, num2;
+
         for (num1 = 0; num1 <= 98; num1++)
         {
                 for (num2 = num1 + 1; num2 <= 99; num2++)
