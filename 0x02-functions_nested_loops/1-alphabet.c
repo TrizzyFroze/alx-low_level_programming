771de4dd3a639183e46fb7d19452b4cf187c9dd6
TrizzyFroze <trizzyfroze2000@gmail.com>
2023-09-13 13:57:39 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,31 +7,14 @@
  */
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
-}
-
-
-1-main.c
-
-
-#include "main.h"
-
-
-/**
- * main - check the code
- * Return: Always 0.
- */
-int main(void)
-{
-        print_alphabet();
-        return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,12 +2,11 @@
 
 
 /**
- * main - check the code.
- *
+ * main - check the code
  * Return: Always 0.
  */
 int main(void)
 {
-print_alphabet_x10();
+print_alphabet();
 return (0);
 }
